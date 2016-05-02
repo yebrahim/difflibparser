528e1692dbb7d6246ea829de585aef2b31d3d143
Yasser Elsayed <yasser.ebrahim@gmail.com>
2016-05-01 23:53:18 -0700

    fix parser to correctly recognize incrementally changed lines

--- difflibparser.py
+++ difflibparser.py
@@ -4,14 +4,13 @@ class DiffCode:
     SIMILAR = 0         # starts with '  '
     RIGHTONLY = 1       # starts with '+ '
     LEFTONLY = 2        # starts with '- '
-    CHANGEDLEFT = 3     # two lines, first starts with '+ ' or '- ' and second with '? '
-    CHANGEDRIGHT = 4    # two lines, first starts with '+ ' or '- ' and second with '? '
+    CHANGED = 3         # either three or four lines with the prefixes ('-', '+', '?'), ('-', '?', '+') or ('-', '?', '+', '?') respectively
 
 class DifflibParser:
     def __init__(self, text1, text2):
         self.__text1 = text1
         self.__text2 = text2
-        self.__diff = list(difflib.ndiff(text1.splitlines(), text2.splitlines()))
+        self.__diff = list(difflib.ndiff(text1, text2))
         self.__currentLineno = 0
 
     def getNextLine(self):
@@ -25,24 +24,54 @@ class DifflibParser:
         if code == '  ':
             result['code'] = DiffCode.SIMILAR
         elif code == '- ':
-            # check if next line indicates an incremental change
-            nextLine = self.__diff[self.__currentLineno + 1] if self.__currentLineno + 1 < len(self.__diff) else None
-            if nextLine and nextLine[:2] == '? ':
-                result['code'] = DiffCode.CHANGEDLEFT
-                self.__currentLineno += 1
-                minusIndices = [i for (i,c) in enumerate(nextLine[2:]) if c in ['-', '^']]
-                result['changedIndices'] = minusIndices
-            else:
+            incrementalChange = self.__tryGetIncrementalChange(self.__currentLineno)
+            if not incrementalChange:
                 result['code'] = DiffCode.LEFTONLY
-        elif code == '+ ':
-            # check if next line indicates an incremental change
-            nextLine = self.__diff[self.__currentLineno + 1] if self.__currentLineno + 1 < len(self.__diff) else None
-            if nextLine and nextLine[:2] == '? ':
-                result['code'] = DiffCode.CHANGEDRIGHT
-                self.__currentLineno += 1
-                plusIndices = [i for (i,c) in enumerate(nextLine[2:]) if c in ['+', '^']]
-                result['changedIndices'] = plusIndices
             else:
-                result['code'] = DiffCode.RIGHTONLY
+                result['code'] = DiffCode.CHANGED
+                result['leftchanges'] = incrementalChange['left'] if 'left' in incrementalChange else None
+                result['rightchanges'] = incrementalChange['right'] if 'right' in incrementalChange else None
+                result['newline'] = incrementalChange['newline']
+                self.__currentLineno += incrementalChange['skiplines']
+        elif code == '+ ':
+            result['code'] = DiffCode.RIGHTONLY
         self.__currentLineno += 1
         return result
+
+
+    def __tryGetIncrementalChange(self, lineno):
+        lineOne = self.__diff[lineno] if lineno < len(self.__diff) else None
+        lineTwo = self.__diff[lineno + 1] if lineno + 1 < len(self.__diff) else None
+        lineThree = self.__diff[lineno + 2] if lineno + 2 < len(self.__diff) else None
+        lineFour = self.__diff[lineno + 3] if lineno + 3 < len(self.__diff) else None
+
+        changes = {}
+        # ('-', '?', '+', '?') case
+        if lineOne and lineOne[:2] == '- ' and \
+           lineTwo and lineTwo[:2] == '? ' and \
+           lineThree and lineThree[:2] == '+ ' and \
+           lineFour and lineFour[:2] == '? ':
+            changes['left'] = [i for (i,c) in enumerate(lineTwo[2:]) if c in ['-', '^']]
+            changes['right'] = [i for (i,c) in enumerate(lineFour[2:]) if c in ['+', '^']]
+            changes['newline'] = lineThree[2:]
+            changes['skiplines'] = 3
+            return changes
+        # ('-', '+', '?')
+        elif lineOne and lineOne[:2] == '- ' and \
+           lineTwo and lineTwo[:2] == '+ ' and \
+           lineThree and lineThree[:2] == '? ':
+            changes['right'] = [i for (i,c) in enumerate(lineThree[2:]) if c in ['+', '^']]
+            changes['newline'] = lineTwo[2:]
+            changes['skiplines'] = 2
+            return changes
+        # ('-', '?', '+')
+        elif lineOne and lineOne[:2] == '- ' and \
+           lineTwo and lineTwo[:2] == '? ' and \
+           lineThree and lineThree[:2] == '+ ':
+            changes['left'] = [i for (i,c) in enumerate(lineTwo[2:]) if c in ['-', '^']]
+            changes['newline'] = lineThree[2:]
+            changes['skiplines'] = 2
+            return changes
+        # no incremental change
+        else:
+            return None
---
+++ tests.py
@@ -0,0 +1,30 @@
+from difflibparser import *
+
+file1 = '''
+line1
+lineTwo
+lineThrees
+line4
+line6
+'''
+
+file2 = '''
+line1
+xlineTw0
+lineThree
+line4
+line66
+'''
+
+diff = difflib.ndiff(file1.splitlines(), file2.splitlines())
+for line in diff:
+    print(line)
+
+print('---------\n')
+
+differ = DifflibParser(file1.splitlines(), file2.splitlines())
+
+line = differ.getNextLine()
+while line:
+    print(line)
+    line = differ.getNextLine()
